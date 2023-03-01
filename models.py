a3981d9841402e004262e65ee4f4500bebf2226e
MSDiehl <diehl.malachi990@gmail.com>
2023-02-28 18:51:07 -0600

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,12 @@
-import tensorflow as tf
-<<<<<<< HEAD
 from attentionMechanisms import *
-=======
->>>>>>> e01ac3fba0b2505e83e37913163f940880a0e460
-from tensorflow.keras.layers import Input, Embedding, Bidirectional, LSTM, Concatenate, Dense, Attention
+
+#Where encoder_input_data, decoder_input_data, and decoder_target_data are your training data
+
+#batch_size, epochs, input_vocab_size, output_vocab_size, input_length, output_length, and n_units are hyperparameters that you can adjust based on your specific task.
 
 model = create_attention_model(input_vocab_size, output_vocab_size, input_length, output_length, n_units)
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
           batch_size=batch_size,
           epochs=epochs,
-<<<<<<< HEAD
           validation_split=0.2)
-=======
-          validation_split=0.2)
->>>>>>> e01ac3fba0b2505e83e37913163f940880a0e460
