11ff87578584a1a36f0b5cf8a6ebb39e37d0e9fc
MSDiehl <diehl.malachi990@gmail.com>
2023-03-02 13:43:52 -0600

    Changed the model a bit

--- model.py
+++ model.py
@@ -1,13 +1,32 @@
 from attentionMechanisms import *
+import json
 import tensorflow as tf
+import numpy as np
+import pandas as pd
+import nltk
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.layers import Input, Embedding, Bidirectional, LSTM, Concatenate, Dense, Attention
+from tensorflow.keras.models import Model
+import matplotlib.pyplot as plt
+
 
 #Where encoder_input_data, decoder_input_data, and decoder_target_data are your training data
 
 #batch_size, epochs, input_vocab_size, output_vocab_size, input_length, output_length, and n_units are hyperparameters that you can adjust based on your specific task.
 
-model = create_attention_model(input_vocab_size, output_vocab_size, input_length, output_length, n_units)
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_split=0.2)
+dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(input_shape,)),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy'])
+
+history = model.fit(dataset.batch(batch_size),
+    epochs=num_epochs,
+    validation_data=(x_test, y_test))
+
+
